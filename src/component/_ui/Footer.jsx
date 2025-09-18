55614da2b63b5ec9e07af1e5358abd39143187ae
sajedul4D <sajedultasin17@gmail.com>
2025-09-19 00:50:38 +0600

    update description

--- src/component/_ui/Footer.jsx
+++ src/component/_ui/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
          <div className='w-[100%] border-b py-4 bg-gray-600'>
             <div className='max-w-[1440px] mx-auto flex items-center justify-between'>
-                
+                Footer
             </div>
         </div>
     );
